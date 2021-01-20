25aa3bda6ef3e4f34a13d7150a20074de2d1beeb
Brandon Hafenrichter <bhafenri@gmail.com~>
2021-01-20 18:16:06 +0100

    Separated Webpack Instances

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const webpack = require("webpack");
 
@@ -97,7 +95,7 @@ module.exports = {
             filename: "./css/[name].css",
             chunkFilename: "./css/[id].css",
         }),
-        // new CompressionPlugin()
+        new CompressionPlugin()
     ],
     optimization: {
         // minimize: true,
---
+++ webpack.prd.js
@@ -0,0 +1,115 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const webpack = require("webpack");
+
+const outputDirectory = "dist";
+
+module.exports = {
+    entry: ["babel-polyfill", "./src/client/index.tsx"],
+    output: {
+        path: path.join(__dirname, outputDirectory),
+        filename: "./js/[name].bundle.js"
+    },
+    devtool: "source-map",
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: [{
+                    loader: "awesome-typescript-loader"
+                }, ],
+                exclude: /node_modules/
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: "./Less",
+                            hmr: process.env.NODE_ENV === "development",
+                        },
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "less-loader",
+                        options: {
+                            strictMath: true,
+                            noIeCompat: true,
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: "url-loader?limit=100000"
+            },
+        ]
+    },
+    resolve: {
+        extensions: ["*", ".ts", ".tsx", ".js", ".jsx", ".json", ".less"]
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+        proxy: {
+            "/api/**": {
+                target: "http://localhost:3001",
+                secure: false,
+                changeOrigin: true
+            }
+        }
+    },
+    plugins: [
+        new webpack.DefinePlugin({ // <-- key to reducing React's size
+            "process.env": {
+                "NODE_ENV": JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
+        new CleanWebpackPlugin([outputDirectory]),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            favicon: "./public/favicon.ico",
+            title: "express-typescript-react",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "./css/[name].css",
+            chunkFilename: "./css/[id].css",
+        }),
+        new CompressionPlugin()
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all"
+                }
+            }
+        }
+    }
+};
